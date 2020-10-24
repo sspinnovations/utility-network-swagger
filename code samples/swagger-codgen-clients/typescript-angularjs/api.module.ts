c1009816ddab95d6f37952c0af194bdb7155faa0
Kim H. Sundeen <ksundeen@SSP.com>
2020-10-23 19:23:53 -0500

    Updated sdk clients that were generated with the updated OpenAPI Spec. Credentials were generalized in the code samples/UtilityNetworkCSharpSample_v2

--- code samples/swagger-codgen-clients/typescript-angularjs/api.module.ts
+++
@@ -1,8 +0,0 @@
-import * as api from './api/api';
-import * as angular from 'angular';
-
-const apiModule = angular.module('api', [])
-.service('DefaultApi', api.DefaultApi)
-.service('UtilityNetworkServiceApi', api.UtilityNetworkServiceApi)
-
-export default apiModule;
--- code samples/swagger-codgen-clients/typescript-angularjs/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './DefaultApi';
-import { DefaultApi } from './DefaultApi';
-export * from './UtilityNetworkServiceApi';
-import { UtilityNetworkServiceApi } from './UtilityNetworkServiceApi';
-export const APIS = [DefaultApi, UtilityNetworkServiceApi];
